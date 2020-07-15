ff5f84b3e145e6be253bea57dc355cd5e10dca1a
ym547559398 <547559398@qq.com>
2020-07-15 16:15:29 +0800

    clean

--- tools/metric/train_metric.py
+++ tools/metric/train_metric.py
@@ -14,7 +14,7 @@ from metric.core.config import cfg
 
 
 def main():
-    config.load_cfg_fom_args("Train a classification model.")
+    config.load_cfg_fom_args("Train a metric model.")
     config.assert_and_infer_cfg()
     cfg.freeze()
     dist.multi_proc_run(num_proc=cfg.NUM_GPUS, fun=trainer.train_model)
